ff2a719ecaf46e6346294229dac738592d82ac0d
mporsyak <47520769+mporsyak@users.noreply.github.com>
2022-01-18 10:43:31 +0300

    create application

---
+++ src/main/java/com/example/spring_eshop/SpringEShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_eshop/SpringEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
